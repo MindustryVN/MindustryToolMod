04721ee85a82416b0d31fe0ed2cfa9c3588b5592
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-12 23:42:24 +0700

    no more error

--- src/mindytool/Main.java
+++ src/mindytool/Main.java
@@ -19,7 +19,6 @@ public class Main extends Mod {
     MapDialog mapDialog;
 
     public Main() {
-        System.load("");
         Events.on(ClientLoadEvent.class, e -> {
             Vars.ui.schematics.buttons.button("Browse", Icon.menu, () -> {
                 Vars.ui.schematics.hide();
