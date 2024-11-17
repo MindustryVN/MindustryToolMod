dab4a75468848189a2edb3d8121bf8232d1ab7bf
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-17 22:38:34 +0700

    remove cache

--- src/mindytool/gui/MapImage.java
+++ src/mindytool/gui/MapImage.java
@@ -7,7 +7,6 @@ import arc.graphics.Texture;
 import arc.graphics.Texture.TextureFilter;
 import arc.graphics.g2d.TextureRegion;
 import arc.scene.ui.Image;
-import arc.struct.ObjectMap;
 import arc.util.Http;
 import arc.util.Http.HttpStatus;
 import arc.util.Http.HttpStatusException;
@@ -23,50 +22,34 @@ public class MapImage extends Image {
     public float thickness = 4f;
     public Color borderColor = Pal.gray;
 
-    private MapData mapData;
-    private TextureRegion lastTexture;
-
-    private static ObjectMap<String, TextureRegion> textureCache = new ObjectMap<>();
-
     public MapImage(MapData mapData) {
         super(Tex.clear);
         setScaling(Scaling.fit);
-        this.mapData = mapData;
+
+        setDrawable(Core.atlas.find("nomap"));
+
+        Http.get(Config.IMAGE_URL + "map-previews/" + mapData.id + ".webp?format=jpeg", res -> {
+            Pixmap pix = new Pixmap(res.getResult());
+            Core.app.post(() -> {
+                try {
+                    var tex = new Texture(pix);
+                    tex.setFilter(TextureFilter.linear);
+                    setDrawable(new TextureRegion(tex));
+                    pix.dispose();
+                } catch (Exception e) {
+                    Log.err(e);
+                }
+            });
+        }, error -> {
+            if (!(error instanceof HttpStatusException requestError) || requestError.status != HttpStatus.NOT_FOUND) {
+                Log.err(error);
+            }
+        });
+
     }
 
     @Override
     public void draw() {
         super.draw();
-
-        // textures are only requested when the rendering happens; this assists with
-        // culling
-        if (!textureCache.containsKey(mapData.id)) {
-            textureCache.put(mapData.id, lastTexture = Core.atlas.find("nomap"));
-
-            Http.get(Config.IMAGE_URL + "map-previews/" + mapData.id + ".webp?format=jpeg", res -> {
-                Pixmap pix = new Pixmap(res.getResult());
-                Core.app.post(() -> {
-                    try {
-                        var tex = new Texture(pix);
-                        tex.setFilter(TextureFilter.linear);
-                        textureCache.put(mapData.id, new TextureRegion(tex));
-                        pix.dispose();
-                    } catch (Exception e) {
-                        Log.err(e);
-                    }
-                });
-            }, error -> {
-                if (!(error instanceof HttpStatusException requestError)
-                        || requestError.status != HttpStatus.NOT_FOUND) {
-                    Log.err(error);
-                }
-            });
-        }
-
-        var next = textureCache.get(mapData.id);
-        if (lastTexture != next) {
-            lastTexture = next;
-            setDrawable(next);
-        }
     }
 }
--- src/mindytool/gui/SchematicImage.java
+++ src/mindytool/gui/SchematicImage.java
@@ -7,7 +7,6 @@ import arc.graphics.Texture;
 import arc.graphics.Texture.TextureFilter;
 import arc.graphics.g2d.TextureRegion;
 import arc.scene.ui.Image;
-import arc.struct.ObjectMap;
 import arc.util.Http;
 import arc.util.Http.HttpStatus;
 import arc.util.Http.HttpStatusException;
@@ -23,50 +22,33 @@ public class SchematicImage extends Image {
     public float thickness = 4f;
     public Color borderColor = Pal.gray;
 
-    private SchematicData schematicData;
-    private TextureRegion lastTexture;
-
-    private static ObjectMap<String, TextureRegion> textureCache = new ObjectMap<>();
-
     public SchematicImage(SchematicData schematicData) {
         super(Tex.clear);
         setScaling(Scaling.fit);
-        this.schematicData = schematicData;
+
+        setDrawable(Core.atlas.find("nomap"));
+
+        Http.get(Config.IMAGE_URL + "schematic-previews/" + schematicData.id + ".webp?format=jpeg", res -> {
+            Pixmap pix = new Pixmap(res.getResult());
+            Core.app.post(() -> {
+                try {
+                    var tex = new Texture(pix);
+                    tex.setFilter(TextureFilter.linear);
+                    setDrawable(new TextureRegion(tex));
+                    pix.dispose();
+                } catch (Exception e) {
+                    Log.err(e);
+                }
+            });
+        }, error -> {
+            if (!(error instanceof HttpStatusException requestError) || requestError.status != HttpStatus.NOT_FOUND) {
+                Log.err(error);
+            }
+        });
     }
 
     @Override
     public void draw() {
         super.draw();
-
-        // textures are only requested when the rendering happens; this assists with
-        // culling
-        if (!textureCache.containsKey(schematicData.id)) {
-            textureCache.put(schematicData.id, lastTexture = Core.atlas.find("nomap"));
-
-            Http.get(Config.IMAGE_URL + "schematic-previews/" + schematicData.id + ".webp?format=jpeg", res -> {
-                Pixmap pix = new Pixmap(res.getResult());
-                Core.app.post(() -> {
-                    try {
-                        var tex = new Texture(pix);
-                        tex.setFilter(TextureFilter.linear);
-                        textureCache.put(schematicData.id, new TextureRegion(tex));
-                        pix.dispose();
-                    } catch (Exception e) {
-                        Log.err(e);
-                    }
-                });
-            }, error -> {
-                if (!(error instanceof HttpStatusException requestError)
-                        || requestError.status != HttpStatus.NOT_FOUND) {
-                    Log.err(error);
-                }
-            });
-        }
-
-        var next = textureCache.get(schematicData.id);
-        if (lastTexture != next) {
-            lastTexture = next;
-            setDrawable(next);
-        }
     }
 }
