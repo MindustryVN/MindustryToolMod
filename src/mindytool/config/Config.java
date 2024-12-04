f2537f63862622d73dd5c2429867a780da67fabd
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-05 00:32:06 +0700

    update domain

--- src/mindytool/config/Config.java
+++ src/mindytool/config/Config.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class Config {
 
-    private static final String DEV_URL = "https://api.mindustry-tool.app/api/v3/";
+    private static final String DEV_URL = "https://api.mindustry-tool.com/api/v3/";
     // private static final String DEV_URL = "http://localhost:8080/api/v3/";
-    private static final String PROD_URL = "https://api.mindustry-tool.app/api/v3/";
+    private static final String PROD_URL = "https://api.mindustry-tool.com/api/v3/";
     // private static final String PROD_URL =
-    // "https://api.mindustry-tool.app/api/v3/";
+    // "https://api.mindustry-tool.com/api/v3/";
     private static final String ENV = System.getenv("ENV");
     public static final boolean DEV = (ENV != null && ENV.equals("DEV"));
     public static final String API_URL = DEV ? DEV_URL : PROD_URL;
-    public static final String IMAGE_URL = "https://image.mindustry-tool.app/";
+    public static final String IMAGE_URL = "https://image.mindustry-tool.com/";
 
     public static final String REPO_URL = "https://api.github.com/repos/sharrlotte/MindustryToolMod/releases/latest";
 
-    public static final String WEB_URL = "https://mindustry-tool.app";
+    public static final String WEB_URL = "https://mindustry-tool.com";
     public static final String UPLOAD_SCHEMATIC_URL = WEB_URL + "/upload/schematic";
     public static final String UPLOAD_MAP_URL = WEB_URL + "/upload/map";
 
