ed7ff65bf34d6a918282c14a4f120196785c3e3a
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-04 22:14:01 +0700

    Remove unused webpToPng method from Utils.java

--- src/mindytool/config/Utils.java
+++ src/mindytool/config/Utils.java
@@ -1,11 +1,5 @@
 package mindytool.config;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.imageio.ImageIO;
-
 import arc.struct.ObjectMap;
 import mindustry.game.Schematic;
 import mindustry.game.Schematics;
@@ -16,17 +10,5 @@ public class Utils {
 
     public static synchronized Schematic readSchematic(String data) {
         return schematicData.get(data, () -> Schematics.readBase64(data));
-    }
-
-    public static byte[] webpToPng(InputStream webpStream) {
-        try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            var image = ImageIO.read(webpStream);
-            ImageIO.write(image, "png", baos);
-            return baos.toByteArray();
-
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to convert image");
-        }
     }
 }
