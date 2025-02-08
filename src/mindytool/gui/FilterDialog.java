6d9f2efcd2a163530f56722fabd01128bb9ab41b
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-02-09 01:08:06 +0700

    some ui tweaks

--- src/mindytool/gui/FilterDialog.java
+++ src/mindytool/gui/FilterDialog.java
@@ -6,6 +6,7 @@ import mindytool.data.TagData;
 import mindytool.data.TagService;
 import arc.Core;
 import arc.func.Cons;
+import arc.graphics.Color;
 import arc.scene.ui.ButtonGroup;
 import arc.scene.ui.TextButton.TextButtonStyle;
 import arc.struct.Seq;
@@ -29,18 +30,19 @@ public class FilterDialog extends BaseDialog {
         TagService.onUpdate(() -> show(searchConfig));
 
         cont.clear();
+        cont.setColor(Color.black);
         cont.pane(table -> {
             table.defaults().minSize(200, 50);
             table.table(Styles.grayPanel, text -> text.add(Core.bundle.format("messages.sort")).left());
 
             var buttonGroup = new ButtonGroup<>();
             table.pane(valueTable -> {
-                valueTable.defaults().size(200, 50);
+                valueTable.defaults().size(300, 50);
                 for (var sort : Config.sorts) {
                     valueTable.button(Core.bundle.format("tags.values." + sort.getName()), style, () -> searchConfig.setSort(sort))///
                             .group(buttonGroup).checked(sort.equals(searchConfig.getSort()));
                 }
-            }).top().left().scrollY(false).marginBottom(12).padBottom(12);
+            }).top().left().scrollY(false).marginBottom(24).padBottom(24);
 
             table.row();
 
@@ -48,11 +50,12 @@ public class FilterDialog extends BaseDialog {
                 for (var tag : schematicTags) {
                     table.table(Styles.grayPanel, text -> text.add(Core.bundle.format("tags.categories." + tag.name()))).top();
                     table.pane(valueTable -> {
-                        valueTable.defaults().size(200, 50);
+                        valueTable.defaults().height(50);
                         for (int i = 0; i < tag.values().size; i++) {
                             var value = tag.values().get(i);
 
                             valueTable.button(btn -> {
+                                btn.left();
                                 if (value.icon() != null && !value.icon().isBlank()) {
                                     btn.add(new NetworkImage(value.icon()))//
                                             .size(24)//
@@ -60,17 +63,19 @@ public class FilterDialog extends BaseDialog {
                                             .marginRight(4);
                                 }
                                 btn.add(Core.bundle.format("tags.values." + value.name()));
-                            }, style, () -> searchConfig.setTag(value.name()))//
-                                    .checked(searchConfig.containTag(value.name()))//
+                            }, style, () -> searchConfig.setTag(tag.name() + "_" + value.name()))//
+                                    .checked(searchConfig.containTag(tag.name() + "_" + value.name()))//
                                     .padRight(4)//
+                                    .padBottom(4)//
                                     .left()//
-                                    .marginRight(4);
+                                    .fillX()//
+                                    .margin(12);
 
-                            if (i > 0 && i % 8 == 0) {
+                            if (i > 0 && i % 6 == 0) {
                                 valueTable.row();
                             }
                         }
-                    }).top().left().scrollY(false).marginBottom(12).padBottom(12);
+                    }).top().left().scrollY(false).marginBottom(24).padBottom(24);
                     table.row();
                 }
             });
