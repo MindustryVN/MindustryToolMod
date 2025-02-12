9595357d317b97643a50347666e792ad30298d3b
sharrlotte <nguyennhonhau25052003@gmail.com>
2025-02-12 18:55:12 +0700

    v1.9.1 fix tool btn not work on mobile

--- src/mindytool/Main.java
+++ src/mindytool/Main.java
@@ -50,15 +50,24 @@ public class Main extends Mod {
                 schematicDialog.show();
             });
 
-            Vars.ui.menufrag.addButton(new MenuButton("Tools", Icon.wrench, () -> {
-            }, //
-                    new MenuButton(Core.bundle.format("map-browser"), Icon.map, () -> {
-                        mapDialog.show();
-                    }), //
-                    new MenuButton(Core.bundle.format("server-browser"), Icon.menu, () -> {
-                        serverDialog.show();
-                    })//
-            ));
+            if (Vars.mobile) {
+                Vars.ui.menufrag.addButton(Core.bundle.format("map-browser"), Icon.map, () -> {
+                    mapDialog.show();
+                });
+                Vars.ui.menufrag.addButton(Core.bundle.format("server-browser"), Icon.menu, () -> {
+                    serverDialog.show();
+                });
+            } else {
+                Vars.ui.menufrag.addButton(new MenuButton("Tools", Icon.wrench, () -> {
+                }, //
+                        new MenuButton(Core.bundle.format("map-browser"), Icon.map, () -> {
+                            mapDialog.show();
+                        }), //
+                        new MenuButton(Core.bundle.format("server-browser"), Icon.menu, () -> {
+                            serverDialog.show();
+                        })//
+                ));
+            }
         });
     }
 
@@ -70,12 +79,14 @@ public class Main extends Mod {
             Jval json = Jval.read(res.getResultAsString());
             String latestVersion = json.getString("tag_name");
             if (!latestVersion.equals(currentVersion)) {
-                Log.info("Mod require update, current version: " + currentVersion + ", latest version: " + latestVersion);
-                Vars.ui.showConfirm(Core.bundle.format("messages.new-version", currentVersion, latestVersion) + "\nDiscord: https://discord.gg/72324gpuCd", () -> {
-                    Core.app.post(() -> {
-                        Vars.ui.mods.githubImportMod(Config.REPO_URL, true);
-                    });
-                });
+                Log.info("Mod require update, current version: " + currentVersion + ", latest version: "
+                        + latestVersion);
+                Vars.ui.showConfirm(Core.bundle.format("messages.new-version", currentVersion, latestVersion)
+                        + "\nDiscord: https://discord.gg/72324gpuCd", () -> {
+                            Core.app.post(() -> {
+                                Vars.ui.mods.githubImportMod(Config.REPO_URL, true);
+                            });
+                        });
             } else {
                 Log.info("Mod up to date");
             }
