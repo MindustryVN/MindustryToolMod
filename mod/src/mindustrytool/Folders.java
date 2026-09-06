61f61d2fbed595651fc86af5b6efb3eb3558e804
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-09-06 18:58:35 +0700

    refactor(folders): use base directory to group folder paths
    introduce a baseDir variable to centralize the parent path for all tool folders, reducing code duplication and simplifying future maintenance

--- mod/src/mindustrytool/Folders.java
+++ mod/src/mindustrytool/Folders.java
@@ -4,11 +4,12 @@ import arc.files.Fi;
 import mindustry.Vars;
 
 public class Folders {
-    public static Fi imageDir = Vars.dataDirectory.child("mindustry-tool-caches");
-    public static Fi mapsDir = Vars.dataDirectory.child("mindustry-tool-maps");
-    public static Fi schematicDir = Vars.dataDirectory.child("mindustry-tool-schematics");
-    public static Fi backgroundsDir = Vars.dataDirectory.child("mindustry-tool-backgrounds");
-    public static Fi musicsDir = Vars.dataDirectory.child("mindustry-tool-musics");
+    public static Fi baseDir = Vars.dataDirectory.child("mindustry-tool");
+    public static Fi imageDir = baseDir.child("caches");
+    public static Fi mapsDir = baseDir.child("maps");
+    public static Fi schematicDir = baseDir.child("schematics");
+    public static Fi backgroundsDir = baseDir.child("backgrounds");
+    public static Fi musicsDir = baseDir.child("musics");
 
     static {
         imageDir.mkdirs();
