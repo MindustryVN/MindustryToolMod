82384f7f972992233ea9483ef0d775b58bc9831f
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-09-12 01:19:31 +0700

    test(graphics): add CircleDrawable image size test
    This test initializes mock arc Core components, creates an Image using CircleDrawable, and verifies that the reported image dimensions match the set size after layout.

--- solim-core/src/test/java/solim/graphics/CircleDrawableTest.java
+++ solim-core/src/test/java/solim/graphics/CircleDrawableTest.java
@@ -23,4 +23,16 @@ class CircleDrawableTest {
 		assertEquals(0f, cd.getTopHeight());
 		assertEquals(0f, cd.getBottomHeight());
 	}
+
+	@Test
+	void testImageLayout() {
+		if (arc.Core.app == null) arc.Core.app = new arc.mock.MockApplication();
+		if (arc.Core.graphics == null) arc.Core.graphics = new arc.mock.MockGraphics();
+		arc.scene.ui.Image img = new arc.scene.ui.Image(CircleDrawable.INSTANCE);
+		img.setSize(12f, 12f);
+		img.layout();
+		System.out.println("DEBUG_IMG_WIDTH: " + img.getImageWidth() + ", DEBUG_IMG_HEIGHT: " + img.getImageHeight());
+		assertEquals(12f, img.getImageWidth());
+		assertEquals(12f, img.getImageHeight());
+	}
 }
