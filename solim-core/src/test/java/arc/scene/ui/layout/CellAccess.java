df7cf132d11a9098ff3afb61907716d8d8ddb584
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-09-13 02:28:54 +0700

    test(ui/layout): add CellAccess test helper
    Add CellAccess utility to access package-private Cell layout fields for unit testing
    Update .gitignore to add leading slashes to mindustry/ and arc/ to only match top-level directories

---
+++ solim-core/src/test/java/arc/scene/ui/layout/CellAccess.java
@@ -0,0 +1,50 @@
+package arc.scene.ui.layout;
+
+/** Helper for unit testing package-private Arc Cell padding fields. */
+public final class CellAccess {
+	private CellAccess() {}
+
+	public static float padTop(Cell<?> cell) {
+		return cell.padTop;
+	}
+
+	public static float padLeft(Cell<?> cell) {
+		return cell.padLeft;
+	}
+
+	public static float padBottom(Cell<?> cell) {
+		return cell.padBottom;
+	}
+
+	public static float padRight(Cell<?> cell) {
+		return cell.padRight;
+	}
+
+	public static int expandX(Cell<?> cell) {
+		return cell.expandX;
+	}
+
+	public static int expandY(Cell<?> cell) {
+		return cell.expandY;
+	}
+
+	public static float fillX(Cell<?> cell) {
+		return cell.fillX;
+	}
+
+	public static float fillY(Cell<?> cell) {
+		return cell.fillY;
+	}
+
+	public static int align(Cell<?> cell) {
+		return cell.align;
+	}
+
+	public static float minWidth(Cell<?> cell) {
+		return cell.minWidth;
+	}
+
+	public static float maxWidth(Cell<?> cell) {
+		return cell.maxWidth;
+	}
+}
